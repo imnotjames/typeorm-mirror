56ab2feb9a119a7e3c4cb8fe1acdb504f814818d
James Ward <james@notjam.es>
2021-07-26 12:39:00 -0400

    add CI code

---
+++ new-version.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MANIFEST_FILE="$1/package.json"
+VERSION_FILE="$1/VERSION"
+GIT_DIRECTORY="$1/.git/"
+
+REPO_NAME="@imnotjames/typeorm"
+REPO_URL="https://github.com/imnotjames/typeorm.git"
+BUGS_URL="https://github.com/imnotjames/typeorm/issues"
+
+PACKAGE_MANIFEST=`cat "$MANIFEST_FILE"`
+
+CURRENT_VERSION=`echo "$PACKAGE_MANIFEST" | jq -r '.version | split("-")[0] // "0.0.0"'`
+
+NEXT_PATCH_VERSION=`echo "$PACKAGE_MANIFEST" | jq -r '.version | split("-")[0] | split(".") | [ .[0] // 0, .[1] // 0, (.[2] | tonumber? // 0) + 1 ] | join(".")'`
+NEXT_MINOR_VERSION=`echo "$PACKAGE_MANIFEST" | jq -r '.version | split("-")[0] | split(".") | [ .[0] // 0, (.[1] | tonumber? // 0) + 1, 0 ] | join(".")'`
+NEXT_MAJOR_VERSION=`echo "$PACKAGE_MANIFEST" | jq -r '.version | split("-")[0] | split(".") | [ (.[0] | tonumber? // 0) + 1, 0, 0 ] | join(".")'`
+GIT_HASH=`git --git-dir "$GIT_DIRECTORY" rev-parse --short HEAD`
+
+RC_SUFFIX="rc.${GIT_HASH}"
+
+IS_RC_ALREADY=`echo "$PACKAGE_MANIFEST" | jq '.version | test("^.*-rc.[a-f0-9]+$")'`
+
+if [[ "$IS_RC_ALREADY" == "true" ]]; then
+    NEXT_VERSION="$CURRENT_VERSION-$RC_SUFFIX"
+else
+    NEXT_VERSION="$NEXT_PATCH_VERSION-$RC_SUFFIX"
+fi
+
+echo $NEXT_VERSION > "$VERSION_FILE"
---
+++ patch-manifest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MANIFEST_FILE="$1/package.json"
+VERSION_FILE="$1/VERSION"
+GIT_DIRECTORY="$1/.git/"
+
+REPO_NAME="@imnotjames/typeorm"
+REPO_URL="https://github.com/imnotjames/typeorm.git"
+BUGS_URL="https://github.com/imnotjames/typeorm/issues"
+
+NEXT_VERSION=`cat "$VERSION_FILE"`
+
+PACKAGE_MANIFEST=`cat "$MANIFEST_FILE"`
+
+PACKAGE_MANIFEST=$(
+    echo "$PACKAGE_MANIFEST" | jq --arg VERSION "$NEXT_VERSION" --arg NAME "$REPO_NAME" --arg REPO_URL "$REPO_URL" --arg BUGS_URL "$BUG_URL" \
+        '.name = $NAME | .version = $VERSION | .repository.url = $REPO_URL | .bugs.url = $BUGS_URL'
+)
+
+PACKAGE_MANIFEST=$(
+    echo "$PACKAGE_MANIFEST" | jq 'del(.author, .funding, .collective)'
+)
+
+echo $PACKAGE_MANIFEST > "$MANIFEST_FILE"
